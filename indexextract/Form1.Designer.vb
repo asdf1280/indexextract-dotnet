d6859172002b8d541dc3384a3fe2d676fd2a368a
Command <admin@userapps.net>
2017-03-12 01:21:43 +0900

    ddd

--- indexextract/Form1.Designer.vb
+++ indexextract/Form1.Designer.vb
@@ -22,10 +22,15 @@ Partial Class Form1
     '코드 편집기에서는 수정하지 마세요.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         Me.SuspendLayout()
         '
         'TextBox1
@@ -41,11 +46,12 @@ Partial Class Form1
         '
         'Button1
         '
+        Me.Button1.Enabled = False
         Me.Button1.Location = New System.Drawing.Point(155, 12)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(180, 23)
         Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Start extract 추출 시작하기"
+        Me.Button1.Text = "Start"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Button2
@@ -54,7 +60,7 @@ Partial Class Form1
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(137, 23)
         Me.Button2.TabIndex = 2
-        Me.Button2.Text = "찾아보기 Browse"
+        Me.Button2.Text = "Browse"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'OpenFileDialog1
@@ -64,16 +70,50 @@ Partial Class Form1
         Me.OpenFileDialog1.Filter = "json|*.json"
         Me.OpenFileDialog1.Title = "Select version json 버전 선택하기"
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(341, 17)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(81, 12)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "현재 작업중 : "
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(12, 353)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "English"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(93, 353)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 5
+        Me.Button4.Text = "한국어"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(584, 355)
+        Me.ClientSize = New System.Drawing.Size(584, 392)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.TextBox1)
         Me.Name = "Form1"
-        Me.Text = "Index Extractor by command dhkim0800"
+        Me.Text = "Index Extractor 2.0"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -83,4 +123,8 @@ Partial Class Form1
     Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
     Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Timer1 As Timer
 End Class
--- indexextract/Form1.vb
+++ indexextract/Form1.vb
@@ -1,17 +1,14 @@
 ﻿Public Class Form1
+    Public i As Integer
+    Public a As Integer = 3
+    Public hash As String
+    Public b As Integer
+    Public file As String
+    Public korean As Boolean = False
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        On Error GoTo b
-        Dim a As Integer = 3
-        Dim hash As String
-        Dim file As String
-a:
-        file = Split(TextBox1.Text, Chr(34))(a)
-        hash = Split(TextBox1.Text, Chr(34))(a + 4)
-        My.Computer.FileSystem.CopyFile("c:\users\" & Split(My.User.Name, "\")(1) & "\appdata\roaming\.minecraft\assets\objects\" & Microsoft.VisualBasic.Left(hash, 2) & "\" & hash, "indexextract\" & file, True)
-        a = a + 8
-        GoTo a
-b:
-        MsgBox("Saved as indexextract   indexextract 에 저장되었습니다.", vbInformation)
+        i = 0
+        a = 3
+        Timer1.Enabled = True
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -19,8 +16,52 @@ b:
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
         On Error Resume Next
         OpenFileDialog1.ShowDialog()
         TextBox1.Text = My.Computer.FileSystem.ReadAllText(OpenFileDialog1.FileName)
+        b = UBound(Split(TextBox1.Text, "hash"))
+        Button1.Enabled = True
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Button2.Text = "Browse"
+        Button1.Text = "Start"
+        korean = True
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Button2.Text = "찾아보기"
+        Button1.Text = "시작"
+        korean = True
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        On Error GoTo b
+
+        i = i + 1
+        If korean Then
+            Label1.Text = "현재 : " & i & "/" & b + 1
+        Else
+            Label1.Text = "Status : " & i & "/" & b + 1
+        End If
+
+        file = Split(TextBox1.Text, Chr(34))(a)
+        hash = Split(TextBox1.Text, Chr(34))(a + 4)
+        'MsgBox(hash & " " & file)
+
+        My.Computer.FileSystem.CopyFile("c:\users\" & Split(My.User.Name, "\")(1) & "\appdata\roaming\.minecraft\assets\objects\" & Microsoft.VisualBasic.Left(hash, 2) & "\" & hash, "indexextract\" & file, True)
+        a = a + 8
+        Exit Sub
+b:
+        If korean Then
+            TextBox1.Text = "완료됨 indexextract\" & OpenFileDialog1.FileName & "\... 에 저장되었습니다."
+        Else
+            TextBox1.Text = "Finished : indexextract\" & OpenFileDialog1.FileName & "\..."
+        End If
+
+        Timer1.Enabled = False
+        Button1.Enabled = False
+        Exit Sub
     End Sub
 End Class
